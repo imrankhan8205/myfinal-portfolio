76a81d969160acc4ffd1b06e56863045f55fe44b
imrankhan8205 <fmd999794@gmail.com>
2025-10-27 23:36:10 +0530

    Moved project files to root directory

---
+++ data/index.ts
@@ -0,0 +1,202 @@
+export const navItems = [
+    { name: "About", link: "#about" },
+    { name: "Projects", link: "#projects" },
+    { name: "Experience", link: "#experience" },
+    { name: "Contact", link: "#contact" },
+];
+
+export const gridItems = [
+    {
+        id: 1,
+        title: "I prioritize client collaboration, fostering open communication ",
+        description: "",
+        className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+        imgClassName: "w-full h-full",
+        titleClassName: "justify-end",
+        img: "/b1.svg",
+        spareImg: "",
+    },
+    {
+        id: 2,
+        title: "I'm very flexible with time zone communications",
+        description: "",
+        className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+        imgClassName: "",
+        titleClassName: "justify-start",
+        img: "",
+        spareImg: "",
+    },
+    {
+        id: 3,
+        title: "My tech stack",
+        description: "I constantly try to improve",
+        className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+        imgClassName: "",
+        titleClassName: "justify-center",
+        img: "",
+        spareImg: "",
+    },
+    {
+        id: 4,
+        title: "Tech enthusiast with a passion for  Digital Marketing & development.",
+        description: "",
+        className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+        imgClassName: "",
+        titleClassName: "justify-start",
+        img: "/grid.svg",
+        spareImg: "/b4.svg",
+    },
+
+    {
+        id: 5,
+        title: "Currently working on  Tech Grow Solutions system & Digital marketing client ( Nawabilazzat a Restaurant)",
+        description: "Include Graphic and video editing with high level of curiosity and making reels over all social media managing ",
+        className: "md:col-span-3 md:row-span-2",
+        imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+        titleClassName: "justify-center md:justify-start lg:justify-center",
+        img: "/b5.svg",
+        spareImg: "/grid.svg",
+    },
+    {
+        id: 6,
+        title: "Do you want to start a project together?",
+        description: "",
+        className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+        imgClassName: "",
+        titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+        img: "",
+        spareImg: "",
+    },
+];
+
+export const projects = [
+    {
+        id: 1,
+        title: "uess_random-number-_game",
+        des: "A web application for guess the Exact Number using mind puzzle and by tracking number behaviour ",
+        img: "/random_numbers2.png",
+        link: "https://guess-random-number-game-seven.vercel.app/",
+    },
+    {
+        id: 2,
+        title: "Mind Repeating game Quick Learning Power and memory Capacity ",
+        des: "A web application that Get your mind fast to remember how the color is going to come and previous record each steps of color occur must be same pattern ",
+        img: "/brain_game.jpg",
+        link: "https://github.com/imrankhan8205/simon_Game",
+    },
+    {
+        id: 3,
+        title: "Digital Marketing at Abhirachna and Tech Grow solution Systems ",
+        des: "Handling all Marketing Strategy and Graphic design and Video in all social Media Platform ",
+        img: "/hello.png",
+        link: "https://www.instagram.com/abhirachnaa",
+    },
+    {
+        id: 4,
+        title: "Digital Marketing Client a Restaurant  NAWABILAZZAT based on Kharar Mohali (Chandigarh)",
+        des: "handling Google ads and meta ads  . Youtube ,Instagram ,facebook with graphic and video editing reels all over social media platform ",
+        img: "/majer.jpg",
+        link: "https://www.instagram.com/nawabilazzat",
+    },
+];
+
+// export const testimonials = [
+//     {
+//         quote:
+//             "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+//         name: "Michael Johnson",
+//         title: "Director of AlphaStream Technologies",
+//     },
+//     {
+//         quote:
+//             "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+//         name: "Michael Johnson",
+//         title: "Director of AlphaStream Technologies",
+//     },
+//     {
+//         quote:
+//             "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+//         name: "Michael Johnson",
+//         title: "Director of AlphaStream Technologies",
+//     },
+//     {
+//         quote:
+//             "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+//         name: "Michael Johnson",
+//         title: "Director of AlphaStream Technologies",
+//     },
+//     {
+//         quote:
+//             "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+//         name: "Michael Johnson",
+//         title: "Director of AlphaStream Technologies",
+//     },
+// ];
+
+export const companies = [
+    {
+        id: 1,
+        name: "cloudinary",
+        img: "/cloud.svg",
+        nameImg: "/cloudName.svg",
+    },
+    {
+        id: 2,
+        name: "appwrite",
+        img: "/app.svg",
+        nameImg: "/appName.svg",
+    },
+    {
+        id: 3,
+        name: "HOSTINGER",
+        img: "/host.svg",
+        nameImg: "/hostName.svg",
+    },
+    {
+        id: 4,
+        name: "stream",
+        img: "/s.svg",
+        nameImg: "/streamName.svg",
+    },
+    {
+        id: 5,
+        name: "docker.",
+        img: "/dock.svg",
+        nameImg: "/dockerName.svg",
+    },
+];
+
+export const workExperience = [
+    {
+        id: 1,
+        title: "DigitaL Marketing At Abhirachnaa Pvt Ltd",
+        desc: "a fully based on interior designer home and flat . My work is digital marketing  + graphic + video editing ",
+        className: "md:col-span-2",
+        thumbnail: "/digi.png",
+    },
+    {
+        id: 2,
+        title: " Live client in Digital Marketing and Web Application Based on Nawabilazzat restaurent website and social media handling including SEO and Performance Marketing ",
+        desc: "Assisted in developing tokenomics for a new cryptocurrency project.",
+        className: "md:col-span-2", // change to md:col-span-2
+        thumbnail: "/1v1.jpg",
+    }
+];
+
+export const socialMedia = [
+    {
+        id: 1,
+        img: "/git.svg",
+        link: "https://github.com/imrankhan8205"
+    },
+    {
+        id: 2,
+        img: "/twit.svg",
+        link: "https://x.com/MdimranKha84321"
+    },
+    {
+        id: 3,
+        img: "/link.svg",
+        link: "https://www.linkedin.com/in/khanimran82/"
+    },
+];
